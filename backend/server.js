6d5bdfe985dd3c274e0c80916a50963d0c23d8fd
Ayushmaurya <ayushmaurya3596@gmail.com>
2025-01-24 19:35:54 +0530

    Add frontend and backend folders

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.get('/', (req, res) => {
 });
 
 // MongoDB Connection
-const mongoURI = process.env.MONGODB_URI || "mongodb+srv://ayush1:Ayush210704@cluster0.in8q5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongoURI = process.env.MONGODB_URI;
 mongoose
   .connect(mongoURI, {
     useNewUrlParser: true,
